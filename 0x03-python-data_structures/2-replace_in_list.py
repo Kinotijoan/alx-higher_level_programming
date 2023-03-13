bcd3752086da8e30094bc03b58756c85d9dc1005
Kinotijoan <kinotijoan@gmail.com>
2023-03-13 07:13:32 -0700

    replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     lenSize = len(my_list)
-    if idx < 0 or idx > lenSize:
+    if idx < 0 or idx >= lenSize:
         return my_list
     else:
         for i, v in enumerate(my_list):
