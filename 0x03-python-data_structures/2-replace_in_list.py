7f5e5f777ea021d3208315248d928833d9ebb7cc
Kinotijoan <kinotijoan@gmail.com>
2023-03-13 07:05:49 -0700

    replace

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    lenSize = len(my_list)
+    if idx < 0 or idx > lenSize:
+        return my_list
+    else:
+        for i, v in enumerate(my_list):
+            i == idx
+            v = element
