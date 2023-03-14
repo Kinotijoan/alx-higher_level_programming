67b0202af8c8a51ea48f308251798cc31bf9194c
Kinotijoan <117837618+Kinotijoan@users.noreply.github.com>
2023-03-14 21:53:33 +0300

    python

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-<<<<<<< HEAD
-    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
-=======
     s = len(sentence)
     a = (s, sentence[0])
     return a
->>>>>>> 8b0b14f75ac72c18d737d93b69dcb40c633b532e
+
