90dd74b4de7e1bce3f691dcffacde7c14ac36d00
Kinotijoan <117837618+Kinotijoan@users.noreply.github.com>
2023-06-23 11:09:23 +0300

    revision

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for i in my_string:
-        if i not in "cC":
-            new_string = " ".join(i)
-    return new_string
+    str = [i for i in my_string if i not in "cC"]
+    return "".join(str)
