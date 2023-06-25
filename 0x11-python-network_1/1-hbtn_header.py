7744c67c0c449538cbaffe44e8042244ef914dc4
Kinotijoan <117837618+Kinotijoan@users.noreply.github.com>
2023-06-25 22:58:08 +0300

    python

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python script that takes in a url and displays the value of the X-Request-Id found in the header"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as request:
+        response = request.info()
+    print(response.get("X-Request-Id"))
