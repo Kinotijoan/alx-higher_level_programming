c5b14406aff61c4572b1354d84e327a1a5ed330e
Kinotijoan <117837618+Kinotijoan@users.noreply.github.com>
2023-06-26 09:37:32 +0300

    revision

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=' ')
-        print()
+    try:
+        for i, _ in enumerate(matrix):
+            c = 0
+            for j, v in enumerate(matrix[i][:-1]):
+                print("{:d}".format(v), end=" ")
+                c = j
+            c = 0 if len(matrix[0]) == 1 else c + 1
+            print("{:d}".format(matrix[i][c]))
+    except IndexError:
+        print("")
