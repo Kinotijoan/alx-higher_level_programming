2634608c56b3f95808f83b3b00f494444fd72177
Kinotijoan <117837618+Kinotijoan@users.noreply.github.com>
2023-07-17 11:59:42 +0300

    corrections on exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except:
+    except ZeroDivisionError:
         result = None
     finally:
+        result = a / b
         print("Inside result: {}".format(result))
     return result
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,17 +2,15 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
+        val = 0
         try:
-            new_list.append(my_list_1[i] / my_list_2[i])
+            val = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            new_list.append(0)
             print("division by 0")
         except TypeError:
-            new_list.append(0)
             print("wrong type")
         except IndexError:
-            new_list.append(0)
             print("out of range")
         finally:
-            pass
+            new_list.append(val)
     return new_list
